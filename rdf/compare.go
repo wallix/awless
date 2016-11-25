1668bcc072cb26abc52ac833ec9fdd6e796989c8
simcap <simcap@fastmail.com>
2016-11-25 18:21:02 +0100

    Cleanup and clarification

--- rdf/compare.go
+++ rdf/compare.go
@@ -11,21 +11,19 @@ import (
 )
 
 func Compare(rootID string, local []*triple.Triple, remote []*triple.Triple) ([]*triple.Triple, []*triple.Triple, error) {
-	var extras []*triple.Triple
-	var missings []*triple.Triple
-	var commons []*triple.Triple
+	var allextras, allmissings, allcommons []*triple.Triple
 
 	ctx := context.Background()
 	localGraph, err := memory.DefaultStore.NewGraph(ctx, "local")
 	if err != nil {
-		return extras, missings, err
+		return allextras, allmissings, err
 	}
 	defer memory.DefaultStore.DeleteGraph(ctx, "local")
 	localGraph.AddTriples(ctx, local)
 
 	remoteGraph, err := memory.DefaultStore.NewGraph(ctx, "remote")
 	if err != nil {
-		return extras, missings, err
+		return allextras, allmissings, err
 	}
 	defer memory.DefaultStore.DeleteGraph(ctx, "remote")
 	remoteGraph.AddTriples(ctx, remote)
@@ -33,54 +31,53 @@ func Compare(rootID string, local []*triple.Triple, remote []*triple.Triple) ([]
 	rootNode, err := node.NewNodeFromStrings("/region", rootID)
 
 	if err != nil {
-		return extras, missings, err
+		return allextras, allmissings, err
 	}
 
 	maxCount := max(len(local), len(remote))
-	nodesCh := make(chan *node.Node, maxCount)
-	nodesCh <- rootNode
+	processing := make(chan *node.Node, maxCount)
 
-	for len(nodesCh) > 0 {
+	processing <- rootNode
+
+	for len(processing) > 0 {
 		select {
-		case node := <-nodesCh:
-			processedExtras, processedMissings, processedCommmons, err := compareTriplesFromNode(node, localGraph, remoteGraph)
+		case node := <-processing:
+			extras, missings, commons, err := compareChildTriplesOf(node, localGraph, remoteGraph)
 			if err != nil {
-				return extras, missings, err
+				return allextras, allmissings, err
 			}
-			extras = append(extras, processedExtras...)
-			missings = append(missings, processedMissings...)
-			commons = append(commons, processedCommmons...)
-			for _, nextNodeToProcess := range processedCommmons {
+			allextras = append(allextras, extras...)
+			allmissings = append(allmissings, missings...)
+			allcommons = append(allcommons, commons...)
+			for _, nextNodeToProcess := range commons {
 				objectNode, err := nextNodeToProcess.Object().Node()
 				if err != nil {
-					return extras, missings, err
+					return allextras, allmissings, err
 				}
-				nodesCh <- objectNode
+				processing <- objectNode
 			}
 		}
 	}
 
-	return extras, missings, nil
+	return allextras, allmissings, nil
 }
 
-func compareTriplesFromNode(rootNode *node.Node, localGraph storage.Graph, remoteGraph storage.Graph) ([]*triple.Triple, []*triple.Triple, []*triple.Triple, error) {
-	var extras []*triple.Triple
-	var missings []*triple.Triple
-	var commons []*triple.Triple
+func compareChildTriplesOf(root *node.Node, localGraph storage.Graph, remoteGraph storage.Graph) ([]*triple.Triple, []*triple.Triple, []*triple.Triple, error) {
+	var extras, missings, commons []*triple.Triple
 
-	localTriples, err := triplesForSubjectAndPredicate(localGraph, rootNode, parentOf)
+	locals, err := triplesForSubjectAndPredicate(localGraph, root, parentOf)
 	if err != nil {
 		return extras, missings, commons, err
 	}
 
-	remoteTriples, err := triplesForSubjectAndPredicate(remoteGraph, rootNode, parentOf)
+	remotes, err := triplesForSubjectAndPredicate(remoteGraph, root, parentOf)
 	if err != nil {
 		return extras, missings, commons, err
 	}
 
-	extras = append(extras, SubstractTriples(localTriples, remoteTriples)...)
-	missings = append(missings, SubstractTriples(remoteTriples, localTriples)...)
-	commons = append(commons, IntersectTriples(localTriples, remoteTriples)...)
+	extras = append(extras, SubstractTriples(locals, remotes)...)
+	missings = append(missings, SubstractTriples(remotes, locals)...)
+	commons = append(commons, IntersectTriples(locals, remotes)...)
 
 	return extras, missings, commons, nil
 }
