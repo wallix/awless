ad67485da3f0eb754e3f7a95b5ebb4e21633de8f
simcap <simcap@fastmail.com>
2016-12-07 11:43:53 +0100

    Suppress double error message display

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/wallix/awless/cmd"
 	"github.com/wallix/awless/config"
 )
 
 func main() {
 	config.CreateDefaultConf()
-
-	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(-1)
-	}
+	cmd.RootCmd.Execute()
 }
