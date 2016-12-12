39f899920b255619fd9f08d7351c32023438d94d
François-Xavier Aguessy <fxaguessy@users.noreply.github.com>
2016-12-12 11:55:10 +0100

    Refactoring stats build and send tests

--- stats/stats_test.go
+++ stats/stats_test.go
@@ -25,7 +25,7 @@ import (
 	"github.com/wallix/awless/rdf"
 )
 
-func TestBuildStats(t *testing.T) {
+func TestStats(t *testing.T) {
 	db, close := newTestDb()
 	defer close()
 
@@ -114,6 +114,8 @@ func TestBuildStats(t *testing.T) {
 	db.AddLog("log msg 1")
 	db.AddLog("log msg 2")
 	db.AddLog("log msg 1")
+	db.AddLog("log msg 3")
+	db.AddLog("log msg 1")
 
 	id, _ := db.GetStringValue(AWLESS_ID_KEY)
 	aId, _ := db.GetStringValue(AWLESS_AID_KEY)
@@ -163,8 +165,9 @@ func TestBuildStats(t *testing.T) {
 			MaxGroupsByLocalPolicies: 2,
 		},
 		Logs: []*Log{
-			{Msg: "log msg 1", Hits: 2, Date: now},
+			{Msg: "log msg 1", Hits: 3, Date: now},
 			{Msg: "log msg 2", Hits: 1, Date: now},
+			{Msg: "log msg 3", Hits: 1, Date: now},
 		},
 	}
 
@@ -198,14 +201,88 @@ func TestBuildStats(t *testing.T) {
 	})
 
 	t.Run("Ignoring timestamps", func(t *testing.T) {
-		sort.Sort(ByDateAndCommand(stats.Commands))
-		sort.Sort(ByDateAndCommand(expected.Commands))
-		sort.Sort(ByDateNameAndType(stats.InstancesStats))
-		sort.Sort(ByDateNameAndType(expected.InstancesStats))
+		sort.Sort(ByCommand(stats.Commands))
+		sort.Sort(ByCommand(expected.Commands))
+		sort.Sort(ByTypeAndName(stats.InstancesStats))
+		sort.Sort(ByTypeAndName(expected.InstancesStats))
 		nullifyTime(stats)
 		nullifyTime(&expected)
 		if got, want := reflect.DeepEqual(stats, &expected), true; got != want {
 			t.Fatalf("got\n%+v\nwant\n%+v\n", *stats, expected)
+		}
+	})
+
+	t.Run("SendStats", func(t *testing.T) {
+		privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		processed := false
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var encrypted EncryptedData
+			if e := json.NewDecoder(r.Body).Decode(&encrypted); e != nil {
+				t.Fatal(e)
+				return
+			}
+			defer r.Body.Close()
+
+			sessionKey, e := rsa.DecryptOAEP(sha256.New(), nil, privateKey, encrypted.Key, nil)
+			if e != nil {
+				t.Fatal(e)
+				return
+			}
+
+			decrypted, e := aesDecrypt(encrypted.Data, sessionKey)
+			if e != nil {
+				t.Fatal(e)
+				return
+			}
+
+			extracted, e := gzip.NewReader(bytes.NewReader(decrypted))
+			if e != nil {
+				t.Fatal(e)
+				return
+			}
+			defer extracted.Close()
+
+			var received Stats
+			if e := json.NewDecoder(extracted).Decode(&received); e != nil {
+				t.Fatal(e)
+				return
+			}
+
+			sort.Sort(ByCommand(received.Commands))
+			sort.Sort(ByCommand(expected.Commands))
+			sort.Sort(ByTypeAndName(received.InstancesStats))
+			sort.Sort(ByTypeAndName(expected.InstancesStats))
+			nullifyTime(&received)
+			nullifyTime(&expected)
+
+			if !reflect.DeepEqual(received, expected) {
+				t.Fatalf("got %+v; want %+v", received, expected)
+			}
+			processed = true
+
+		}))
+		defer ts.Close()
+
+		if err = db.SendStats(ts.URL, privateKey.PublicKey, infra, access); err != nil {
+			t.Fatal(err)
+		}
+
+		if got, want := processed, true; got != want {
+			t.Fatalf("got %t; want %t", got, want)
+		}
+
+		logs, err := db.GetLogs()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got, want := len(logs), 0; got != want {
+			t.Fatalf("got %d; want %d", got, want)
 		}
 	})
 }
@@ -243,163 +320,6 @@ func TestBuildMetrics(t *testing.T) {
 
 	if got, want := reflect.DeepEqual(infraMetrics, expectedMetrics), true; got != want {
 		t.Fatalf("got \n%#v\n; want \n%#v\n", infraMetrics, expectedMetrics)
-	}
-}
-
-func TestSendStats(t *testing.T) {
-	db, close := newTestDb()
-	defer close()
-
-	db.AddHistoryCommand([]string{"awless sync"})
-	db.AddHistoryCommand([]string{"awless diff"})
-	db.AddHistoryCommand([]string{"awless diff"})
-	db.AddHistoryCommand([]string{"awless diff"})
-	db.AddHistoryCommand([]string{"awless sync"})
-	db.AddHistoryCommand([]string{"awless list instances"})
-	db.AddHistoryCommand([]string{"awless list vpcs"})
-	db.AddHistoryCommand([]string{"awless list subnets"})
-	db.AddHistoryCommand([]string{"awless list instances"})
-
-	awsInfra := &api.AwsInfra{}
-
-	awsInfra.Instances = []*ec2.Instance{
-		&ec2.Instance{InstanceId: aws.String("inst_1"), SubnetId: aws.String("sub_1"), VpcId: aws.String("vpc_1"), InstanceType: aws.String("t2.micro"), ImageId: aws.String("ami-e98bd29a")},
-		&ec2.Instance{InstanceId: aws.String("inst_2"), SubnetId: aws.String("sub_2"), VpcId: aws.String("vpc_1"), InstanceType: aws.String("t2.micro"), ImageId: aws.String("ami-9398d3e0")},
-	}
-
-	awsInfra.Vpcs = []*ec2.Vpc{
-		&ec2.Vpc{VpcId: aws.String("vpc_1")},
-	}
-
-	awsInfra.Subnets = []*ec2.Subnet{
-		&ec2.Subnet{SubnetId: aws.String("sub_1"), VpcId: aws.String("vpc_1")},
-		&ec2.Subnet{SubnetId: aws.String("sub_2"), VpcId: aws.String("vpc_1")},
-	}
-
-	awsAccess := &api.AwsAccess{}
-
-	awsAccess.Groups = []*iam.Group{
-		&iam.Group{GroupId: aws.String("group_1"), GroupName: aws.String("ngroup_1")},
-	}
-
-	awsAccess.LocalPolicies = []*iam.Policy{
-		&iam.Policy{PolicyId: aws.String("policy_1"), PolicyName: aws.String("npolicy_1")},
-	}
-
-	awsAccess.Roles = []*iam.Role{
-		&iam.Role{RoleId: aws.String("role_1")},
-	}
-
-	awsAccess.Users = []*iam.User{
-		&iam.User{UserId: aws.String("usr_1")},
-		&iam.User{UserId: aws.String("usr_2")},
-	}
-
-	awsAccess.UsersByGroup = map[string][]string{
-		"group_1": []string{"usr_1", "usr_2"},
-	}
-
-	awsAccess.UsersByLocalPolicies = map[string][]string{
-		"policy_1": []string{"usr_1"},
-	}
-
-	awsAccess.RolesByLocalPolicies = map[string][]string{
-		"policy_1": []string{"role_1"},
-	}
-
-	awsAccess.GroupsByLocalPolicies = map[string][]string{
-		"policy_1": []string{"group_1"},
-	}
-
-	localInfra, err := rdf.BuildAwsInfraGraph("eu-west-1", awsInfra)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	localAccess, err := rdf.BuildAwsAccessGraph("eu-west-1", awsAccess)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	db.AddLog("log msg 1")
-	db.AddLog("log msg 2")
-	db.AddLog("log msg 1")
-
-	expected, _, err := BuildStats(db, localInfra, localAccess, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	processed := false
-
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var encrypted EncryptedData
-		if e := json.NewDecoder(r.Body).Decode(&encrypted); e != nil {
-			t.Fatal(e)
-			return
-		}
-		defer r.Body.Close()
-
-		sessionKey, e := rsa.DecryptOAEP(sha256.New(), nil, privateKey, encrypted.Key, nil)
-		if e != nil {
-			t.Fatal(e)
-			return
-		}
-
-		decrypted, e := aesDecrypt(encrypted.Data, sessionKey)
-		if e != nil {
-			t.Fatal(e)
-			return
-		}
-
-		extracted, e := gzip.NewReader(bytes.NewReader(decrypted))
-		if e != nil {
-			t.Fatal(e)
-			return
-		}
-		defer extracted.Close()
-
-		var received Stats
-		if e := json.NewDecoder(extracted).Decode(&received); e != nil {
-			t.Fatal(e)
-			return
-		}
-
-		sort.Sort(ByDateAndCommand(received.Commands))
-		sort.Sort(ByDateAndCommand(expected.Commands))
-		sort.Sort(ByDateNameAndType(received.InstancesStats))
-		sort.Sort(ByDateNameAndType(expected.InstancesStats))
-		nullifyTime(&received)
-		nullifyTime(expected)
-
-		if !reflect.DeepEqual(&received, expected) {
-			t.Fatalf("got %+v; want %+v", &received, expected)
-		}
-		processed = true
-
-	}))
-	defer ts.Close()
-
-	if err = db.SendStats(ts.URL, privateKey.PublicKey, localInfra, localAccess); err != nil {
-		t.Fatal(err)
-	}
-
-	if got, want := processed, true; got != want {
-		t.Fatalf("got %t; want %t", got, want)
-	}
-
-	logs, err := db.GetLogs()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if got, want := len(logs), 0; got != want {
-		t.Fatalf("got %d; want %d", got, want)
 	}
 }
 
@@ -465,16 +385,8 @@ func nullifyTime(i interface{}) {
 		nullifyTime(ii.InfraMetrics)
 		nullifyTime(ii.InstancesStats)
 		nullifyTime(ii.Logs)
-	case *AccessMetrics:
-		ii.Date = time.Time{}
-	case *InfraMetrics:
-		ii.Date = time.Time{}
-	case *DailyCommands:
-		ii.Date = time.Time{}
-	case *InstancesStat:
-		ii.Date = time.Time{}
-	case *Log:
-		ii.Date = time.Time{}
+	case *AccessMetrics, *InfraMetrics, *DailyCommands, *InstancesStat, *Log:
+		reflect.ValueOf(i).Elem().FieldByName("Date").Set(reflect.ValueOf(time.Time{}))
 	case []*DailyCommands:
 		for _, v := range ii {
 			nullifyTime(v)
@@ -492,19 +404,19 @@ func nullifyTime(i interface{}) {
 	}
 }
 
-type ByDateAndCommand []*DailyCommands
+type ByCommand []*DailyCommands
 
-func (a ByDateAndCommand) Len() int      { return len(a) }
-func (a ByDateAndCommand) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a ByDateAndCommand) Less(i, j int) bool {
+func (a ByCommand) Len() int      { return len(a) }
+func (a ByCommand) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByCommand) Less(i, j int) bool {
 	return a[i].Command < a[j].Command
 }
 
-type ByDateNameAndType []*InstancesStat
+type ByTypeAndName []*InstancesStat
 
-func (a ByDateNameAndType) Len() int      { return len(a) }
-func (a ByDateNameAndType) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a ByDateNameAndType) Less(i, j int) bool {
+func (a ByTypeAndName) Len() int      { return len(a) }
+func (a ByTypeAndName) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByTypeAndName) Less(i, j int) bool {
 	if a[i].Type == a[j].Type {
 		return a[i].Name < a[j].Name
 	}
