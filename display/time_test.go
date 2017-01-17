6eda71b6477bf50bd2d6f0d56d85b98747ecadf3
François-Xavier Aguessy <fxaguessy@users.noreply.github.com>
2017-01-17 15:46:56 +0100

    Refactor time formatting tests

--- display/time_test.go
+++ display/time_test.go
@@ -6,37 +6,26 @@ import (
 )
 
 func TestHumanizeTime(t *testing.T) {
-	if got, want := humanizeTime(time.Now()), "now"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
+	tcases := []struct {
+		stamp  time.Time
+		expect string
+	}{
+		{stamp: time.Now(), expect: "now"},
+		{stamp: time.Now().Add(-5 * time.Second), expect: "5 seconds ago"},
+		{stamp: time.Now().Add(-1 * time.Minute), expect: "60 seconds ago"},
+		{stamp: time.Now().Add(-3 * time.Minute), expect: "3 minutes ago"},
+		{stamp: time.Now().Add(-90 * time.Minute), expect: "90 minutes ago"},
+		{stamp: time.Now().Add(-3 * time.Hour), expect: "3 hours ago"},
+		{stamp: time.Now().Add(-24 * time.Hour), expect: "24 hours ago"},
+		{stamp: time.Now().Add(-3 * 24 * time.Hour), expect: "3 days ago"},
+		{stamp: time.Now().Add(-3 * 7 * 24 * time.Hour), expect: "3 weeks ago"},
+		{stamp: time.Now().Add(-3 * 30 * 24 * time.Hour), expect: "3 months ago"},
+		{stamp: time.Now().Add(-3 * 365 * 24 * time.Hour), expect: "3 years ago"},
 	}
-	if got, want := humanizeTime(time.Now().Add(-5*time.Second)), "5 seconds ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if got, want := humanizeTime(time.Now().Add(-1*time.Minute)), "60 seconds ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if got, want := humanizeTime(time.Now().Add(-3*time.Minute)), "3 minutes ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if got, want := humanizeTime(time.Now().Add(-90*time.Minute)), "90 minutes ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if got, want := humanizeTime(time.Now().Add(-3*time.Hour)), "3 hours ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if got, want := humanizeTime(time.Now().Add(-24*time.Hour)), "24 hours ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if got, want := humanizeTime(time.Now().Add(-3*24*time.Hour)), "3 days ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if got, want := humanizeTime(time.Now().Add(-14*24*time.Hour)), "2 weeks ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if got, want := humanizeTime(time.Now().Add(-3*30*24*time.Hour)), "3 months ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if got, want := humanizeTime(time.Now().Add(-2.1*365*24*time.Hour)), "2 years ago"; got != want {
-		t.Fatalf("got %s, want %s", got, want)
+
+	for _, tcase := range tcases {
+		if got, want := humanizeTime(tcase.stamp), tcase.expect; got != want {
+			t.Fatalf("got %s, want %s", got, want)
+		}
 	}
 }
