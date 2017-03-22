34e393e0af437020fb6ec467ccf53a4c98995b76
simcap <simcap@fastmail.com>
2017-03-22 09:52:39 +0100

    whoami now returns policies attached to user

--- commands/whoami.go
+++ commands/whoami.go
@@ -18,9 +18,13 @@ package commands
 
 import (
 	"fmt"
+	"regexp"
 
+	awssdk "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/spf13/cobra"
 	"github.com/wallix/awless/aws"
+	"github.com/wallix/awless/logger"
 )
 
 func init() {
@@ -30,13 +34,80 @@ func init() {
 var whoamiCmd = &cobra.Command{
 	Use:               "whoami",
 	Aliases:           []string{"who"},
-	PersistentPreRun:  applyHooks(initAwlessEnvHook, initCloudServicesHook),
+	PersistentPreRun:  applyHooks(initAwlessEnvHook, initLoggerHook, initCloudServicesHook),
 	PersistentPostRun: applyHooks(saveHistoryHook, verifyNewVersionHook),
-	Short:             "Show your identity",
+	Short:             "Show your account, attached (i.e. managed) and inlined policies",
 
 	Run: func(cmd *cobra.Command, args []string) {
+		me := &whoami{}
+
 		resp, err := aws.SecuAPI.GetCallerIdentity(nil)
 		exitOn(err)
-		fmt.Println(resp)
+
+		me.Account = awssdk.StringValue(resp.Account)
+		me.Arn = awssdk.StringValue(resp.Arn)
+		me.UserId = awssdk.StringValue(resp.UserId)
+
+		username := me.GetUsername()
+
+		policies, err := aws.AccessService.(*aws.Access).ListUserPolicies(&iam.ListUserPoliciesInput{
+			UserName: awssdk.String(username),
+		})
+		if err != nil {
+			logger.Error(err)
+		} else {
+			for _, name := range policies.PolicyNames {
+				me.InlinedPolicies = append(me.InlinedPolicies, awssdk.StringValue(name))
+			}
+		}
+
+		attached, err := aws.AccessService.(*aws.Access).ListAttachedUserPolicies(&iam.ListAttachedUserPoliciesInput{
+			UserName: awssdk.String(username),
+		})
+		if err != nil {
+			logger.Error(err)
+		} else {
+			for _, pol := range attached.AttachedPolicies {
+				me.AttachedPolicies = append(me.AttachedPolicies, policy{Arn: awssdk.StringValue(pol.PolicyArn), Name: awssdk.StringValue(pol.PolicyName)})
+			}
+		}
+
+		fmt.Printf("Username: %s, Id: %s, Account: %s\n", username, me.UserId, me.Account)
+		if len(me.AttachedPolicies) > 0 {
+			fmt.Println("\nAttached policies (i.e. managed):")
+			for _, p := range me.AttachedPolicies {
+				fmt.Printf("\t- %s\n", p.Name)
+			}
+		} else {
+			fmt.Println("\nAttached policies (i.e. managed): none")
+		}
+		if len(me.InlinedPolicies) > 0 {
+			fmt.Println("\nInlined policies:")
+			for _, p := range me.InlinedPolicies {
+				fmt.Printf("\t- %s\n", p)
+			}
+		} else {
+			fmt.Println("\nInlined policies: none")
+		}
 	},
 }
+
+type policy struct {
+	Arn, Name string
+}
+
+type whoami struct {
+	Account, Arn, UserId string
+	AttachedPolicies     []policy
+	InlinedPolicies      []string
+}
+
+func (w *whoami) GetUsername() string {
+	matches := usernameRegex.FindStringSubmatch(w.Arn)
+	if len(matches) != 2 {
+		return ""
+	}
+	return matches[1]
+}
+
+var usernameRegex = regexp.MustCompile(`:user/([\w-.]*)$`)
