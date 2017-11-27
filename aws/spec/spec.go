60ad6c09910c217b7f5f91a866751b8e7258546b
Taras Postument <tpostument@getsocial.im>
2017-11-28 00:50:28 +0200

    fix round to seconds

--- aws/spec/spec.go
+++ aws/spec/spec.go
@@ -392,8 +392,7 @@ func (c *checker) check() error {
 			return nil
 		}
 		elapsed := time.Since(now)
-
-		c.logger.InteractiveInfof("%s %s '%s', expect '%s', timeout in %s (retry in %s)", c.description, c.checkName, got, c.expect, color.New(color.FgGreen).Sprint(c.timeout-elapsed), c.frequency)
+		c.logger.InteractiveInfof("%s %s '%s', expect '%s', timeout in %s (retry in %s)", c.description, c.checkName, got, c.expect, color.New(color.FgGreen).Sprint((c.timeout - elapsed).Seconds()), c.frequency)
 		time.Sleep(c.frequency)
 	}
 }
